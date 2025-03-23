5f47539e680334f658a78013edc7e8aa3c44663d
ErDmYLmz <erdemyilmaz4@gmail.com>
2025-03-23 03:33:34 +0100

    refactor framework_7

--- src/test/java/org/ahassignment/runners/FailedTestCaseRunner.java
+++ src/test/java/org/ahassignment/runners/FailedTestCaseRunner.java
@@ -8,13 +8,13 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "pretty",
-                "html:target/test_reports/cucumber_reports/cucumber_reports.html",
-                "json:target/test_reports/json_reports/cucumber.json",
-                "junit:target/test_reports/xml_report/cucumber.xml",
+                "html:target/test-reports/cucumber-reports/cucumber-reports.html",
+                "json:target/test-reports/json-reports/cucumber.json",
+                "junit:target/test-reports/xml-report/cucumber.xml",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 
         },
-        features = "@target/failed_tests.txt",
+        features = "@target/failed-tests.txt",
         glue = {"org.ahassignment.stepDefinitions"}
 )
 public class FailedTestCaseRunner {
--- src/test/java/org/ahassignment/runners/TestRunner.java
+++ src/test/java/org/ahassignment/runners/TestRunner.java
@@ -9,16 +9,16 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "pretty",
-                "html:target/test_reports/cucumber_reports/cucumber_reports.html",
-                "json:target/test_reports/json_reports/cucumber.json",
-                "junit:target/test_reports/xml_report/cucumber.xml",
+                "html:target/test-reports/cucumber-reports/cucumber-reports.html",
+                "json:target/test-reports/json-reports/cucumber.json",
+                "junit:target/test-reports/xml-report/cucumber.xml",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "rerun:target/failed_tests.txt",
+                "rerun:target/failed-tests.txt",
 
         },
         features = "src/test/resources/features",
         glue = {"org.ahassignment.stepDefinitions"},
-        tags = "@test"
+        tags = "@test1"
 )
 public class TestRunner {
 
